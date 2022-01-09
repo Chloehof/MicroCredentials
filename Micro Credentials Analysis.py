f5a9bcee286c7f6a098b30a71b8d832c63b1e3fe
chloe <chloe.hofstede@gmail.com>
2022-01-09 16:49:41 +0000

    "amended course CSV, add requirements txt, created for loop with if else statement to determine and save result in the awarding body type column, visualisations, dropped duplicates, "

--- Micro Credentials Analysis.py
+++ Micro Credentials Analysis.py
@@ -10,13 +10,12 @@ import os
 from bs4 import BeautifulSoup
 
 
-
 # importing data file CSV format, rename csv
 
 current_dir_path = os.path.dirname(os.path.realpath(__file__))
 courses = pd.read_csv(os.path.join(current_dir_path, 'Courses.csv'), encoding='cp1252')
 
-# importing data using webscraping methode
+# importing data using webscraping methode (requests and beautifulsoup to parse HTML data)
 home_page = requests.get(url='https://www.jobs.ie').text
 soup = BeautifulSoup(home_page, 'html.parser')
 all_categories = soup.find_all("section", {"class": "jobs-by-category accordion"})[0].find_all("a")
@@ -31,21 +30,29 @@ for link in all_categories:
     category_names.append(category_text)
     category_counts.append(category_count)
 
+# converting lists into a dictionary to allow me to reference key values
 output_dict = {"category_text": category_names, "category_count": category_counts}
-
 scraped_df = pd.DataFrame.from_dict(output_dict)
-scraped_df["category_count"] = scraped_df["category_count"].astype(float)
-degree = [True, True, True, True, True, False, False, False, True, False, True, True, True, False, True,
-          False, True, False, True, True, True, True, True, True, False, False, True, True, True, True,
-          False, False, False, False, True, False, True, False, True, True, False, False, False, True, False]
+
+# adding new column 'Degree' in order to sort sectors most likely to benefit from a credit-bearing course
+degree = ['True', 'True', 'True', 'True', 'True', 'False', 'False', 'False', 'True', 'False', 'True', 'True', 'True',
+          'False', 'True', 'False', 'True', 'False', 'True', 'True', 'True', 'True', 'True', 'True', 'False', 'False',
+          'True', 'True', 'True', 'True', 'False', 'False', 'False', 'False', 'True', 'False', 'True', 'False',
+          'True', 'True', 'False', 'False', 'False', 'True', 'False']
 scraped_df['Degree'] = degree
+vacancies_df = scraped_df.loc[scraped_df['Degree'] == 'True']
 
-scraped_df_sorted = scraped_df.sort_values('category_count', ascending=False, inplace=True)
+# In order to sort my category_count values, I needed to convert the data type to a float
+vacancies_df["category_count"] = vacancies_df["category_count"].astype(float)
+
 # data cleaning
+courses_df = pd.DataFrame(courses, columns=['ID', 'CourseID', 'Title', 'Awarding_Body', 'School', 'Credit', 'skills'])
+print("There are " + str(courses_df.duplicated(subset=['CourseID', 'Title']).sum()) + " duplicates in this dataframe.")
 
-
+clean_courses_df = courses_df.drop_duplicates(subset=['CourseID', 'Title'], keep='first')
+print("There are " + str(clean_courses_df.duplicated(subset=['CourseID', 'Title']).sum()) +
+      " duplicates after the drop function was used.")
 # data analysis
-courses_df = pd.DataFrame(courses, columns=['CourseID', 'skills'])
 
 
 # group courses by school/department and group by awarding body
@@ -55,21 +62,37 @@ courses_df = pd.DataFrame(courses, columns=['CourseID', 'skills'])
 # print(sector_subset)
 
 # creating a for loop
-for type, row in courses.iterrows():
-    if "Institute of Technology" in row ['Awarding Body']:
-        courses.loc[type, "Awarding Body Type"] = "IoT"
+for Type, row in courses.iterrows():
+    if "Institute of Technology" in row['Awarding_Body']:
+        courses.loc[Type, "Awarding Body Type"] = "IoT"
     else:
-        courses.loc[type,"Awarding Body Type"] ='University'
+        courses.loc[Type, "Awarding Body Type"] = 'University'
 
 courses.to_csv(r'courses_type.csv')
-courses.head()
+
 # data visualisation
 
-sns.barplot(x='category_count',
-            y='category_text',
-
-            data=scraped_df)
+# visuals for Vacancy data
+sns.barplot(x="category_count",
+            y="category_text",
+            data=vacancies_df,
+            order=vacancies_df.sort_values('category_count',ascending = False).category_text)
+plt.xlabel("Vacancies", size=15)
+plt.ylabel("Sector's", size=15)
+plt.title("Number of vacancies per sector", size=18)
+plt.savefig("VacanciesPerSector.png", dpi=100)
 plt.show()
+
+# visuals for course data
+sns.countplot(y="Awarding_Body",
+              data=clean_courses_df)
+plt.xlabel("# of Courses", size=15)
+plt.ylabel("Awarding Body", size=15)
+plt.title("Number of courses per awarding body", size=18)
+plt.savefig("CoursesPerAwardingBody.png", dpi=100)
+plt.show()
+
+sns.displot(data=clean_courses_df, y="Awarding_Body")
+plt.show()
+
 # analysis conclusion
-
-
